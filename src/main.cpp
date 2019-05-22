9e987dd4262e0e5af4a6ec2f1abbc1b36c78d7ee
Jonathan Plasse <jonathan.plasse@live.fr>
2019-05-22 20:38:42 +0200

    Print Hello World...

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,16 @@
 #include <Arduino.h>
-#include "binary_serial.hpp"
 
-// int c = 0;
-
-void setup() {
-  Serial.begin(115200);
+void setup()
+{
+  Serial.begin(9600); // USB is always 12 Mbit/sec
   pinMode(13, OUTPUT);
   digitalWrite(13, HIGH);
+  delay(5000);
+  digitalWrite(13, LOW);
 }
 
-void loop() {
-  Serial.println("Test");
-  delay(1000);
+void loop()
+{
+  Serial.println("Hello World...");
+  delay(1000);  // do not print too fast!
 }
