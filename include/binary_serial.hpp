94682cca77d324306368800017f46f694c2b4e56
Jonathan Plasse <jonathan.plasse@live.fr>
2019-05-29 14:41:37 +0200

    add non-blocking read_data_if

--- include/binary_serial.hpp
+++ include/binary_serial.hpp
@@ -3,7 +3,13 @@
 
 #include <Arduino.h>
 
+// Wait until data is read
 void read_data(const void* data, const size_t nb_bytes);
+
+// Write data
 void write_data(const void* data, const size_t nb_bytes);
+
+// Read data if available
+bool read_data_if(const void* data, const size_t nb_bytes);
 
 #endif
--- src/binary_serial.cpp
+++ src/binary_serial.cpp
@@ -9,3 +9,11 @@ void write_data(const void* data, const size_t nb_bytes) {
   char* byte_data = (char*) data;
   Serial.write(byte_data, nb_bytes);
 }
+
+bool read_data_if(const void* data, const size_t nb_bytes) {
+  if (Serial.available() < nb_bytes) {
+    return false;
+  }
+  Serial.readBytes((char*) data, nb_bytes);
+  return true;
+}
