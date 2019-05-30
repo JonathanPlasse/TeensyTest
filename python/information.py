25e1f18e29f5301b5b3dac3091c56526c4bdc8a7
Jonathan Plasse <jonathan.plasse@live.fr>
2019-05-30 22:47:42 +0200

    Add comments on information.py

--- python/information.py
+++ python/information.py
@@ -11,9 +11,18 @@ def print_odometry():
 
 
 if __name__ == "__main__":
+    # Initialize serial
     bser = BinSerial("/dev/ttyACM1", 9600)
+
+    # Prepare odometry thread
     odometry_thread = threading.Thread(target=print_odometry, daemon=True)
+
+    # Sent initial position to the robot (True)
     bser.write(['float']*3+['bool'], [0, 0, 0, True])
+
+    # Start odometry thread
     odometry_thread.start()
+
     while True:
+        # Sent setpoint position to the robot (False)
         bser.write(['float']*3+['bool'], [float(input('x: ')), 0, 0, False])
